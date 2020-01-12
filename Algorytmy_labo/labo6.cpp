9fd43c83c176130cfde745bc3c6f46b7f17c8838
lou000 <lou000@users.noreply.github.com>
2020-01-12 09:00:25 +0100

    skonczone, oddane

--- Algorytmy_labo/labo6.cpp
+++ Algorytmy_labo/labo6.cpp
@@ -2,6 +2,8 @@
 #include <random>
 #include <math.h>
 #include <stdio.h>
+#include <QTime>
+
 class Example
 {
 public:
@@ -10,32 +12,43 @@ public:
 
 bool labo6()
 {
+    QTime t;
+    const int MAX_ORDER = 7;
     std::default_random_engine generator;
     std::uniform_int_distribution<int> distribution(20000,40000);
 
     auto lambda = [](const Example& x, const Example& y){return x.integer<y.integer;};
-    MaxHeap<Example, decltype(lambda)> heap(60, lambda);
-    Example ex;
-    ex.integer = 5;
-    heap.add_element(ex);
-    heap.print();
-    heap.clear();
-    for(int i=0; i<50; i++)
+    MaxHeap<Example, decltype(lambda)> heap(static_cast<uint64_t>(pow(10, MAX_ORDER)), lambda);
+    t.start();
+    for(int i=0; i<= MAX_ORDER; i++)
     {
-        Example temp;
-        temp.integer = distribution(generator);
-        heap.add_element(temp);
-    }
-
-    uint64_t index = 0;
-    for(int i=1; i<log2(50+1); i++)
-    {
-        for(int j=0; j<pow(2, (i-1));j++)
+        const int n = static_cast<int>(pow(10, i));
+        t.restart();
+        for(int j=0; j<n; j++)
         {
-            printf("%d ", heap.get(index).integer);
-            index++;
+            Example ex;
+            ex.integer = distribution(generator);
+            heap.add_element(ex);
         }
-        printf("\n");
+        qDebug()<<"Dodano"<<n<<"elementow"<<"w czasie"<<t.elapsed()<<"ms";
+//      WYPISYWANIE DO KONSOLI TEJ LICZBY ELEMENTOW JEST ZBYT WOLNE
+//        uint64_t index = 0;
+//        for(int i=1; i<log2(n+1); i++)
+//        {
+//            for(int j=0; j<pow(2, (i-1))-1;j++)
+//            {
+//                printf("%d ", heap.get(index).integer);
+//                index++;
+//            }
+//            printf("\n");
+//        }
+        t.restart();
+        for(int j=0; j<n; j++)
+        {
+            heap.pop();
+        }
+        qDebug()<<"Usunieto"<<n<<"elementow"<<"w czasie"<<t.elapsed()<<"ms";
+        heap.clear();
     }
     return true;
 }
