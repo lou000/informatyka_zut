5298ada71e69d2b3ad3ff160cba1cc51b8756219
lou000 <lewickimaciej1@gmail.com>
2020-06-05 16:33:27 +0200

    lab5 skonczone

--- PiAD/lab5.py
+++ PiAD/lab5.py
@@ -1,6 +1,8 @@
+from sklearn.datasets import fetch_rcv1
 import numpy as np
 import scipy.sparse as sc
 import pandas as pd
+
 import math
 
 
@@ -12,7 +14,7 @@ def freq(x, prob=True):
         for i, y in enumerate(count[0]):
             if y == cell:
                 found = True
-                cell[i] += 1
+                count[1][i] += 1
         if not found:
             count[0].append(cell)
             count[1].append(1)
@@ -55,7 +57,7 @@ def entropy(x):
     _sum = 0
     idc, y = freq(x)
     for p in y:
-        _sum -= p*math.log(p, 2)
+        _sum -= p * math.log(p, 2)
     return _sum
 
 
@@ -63,7 +65,7 @@ def info_gain(x, y):
     _sum = 0
     idc, z = freq2(x, y)
     for p in z:
-        _sum -= p*math.log(p, 2)
+        _sum -= p * math.log(p, 2)
     return entropy(x) + entropy(y) - _sum
 
 
@@ -74,4 +76,23 @@ data = [[], []]
 
 for col in zoo.columns:
     data[0].append(col)
-    data[1].append(info_gain(col))
+    data[1].append(info_gain(zoo[col], zoo['type']))
+print(np.array(data))
+
+# 5
+# Jeżeli dobrze rozumiem co to znaczy atrybut rzadki to moje funkcje działają na nich domyślnie
+# Jeżeli w poleceniu chodziło o to żeby przekazać do funkcji obiekt np. csc_matrix z pakietu scipy.sparse
+# to powinno być to sprecyzowane.
+# Dla przyjmowanych danych typ zmiennych rzadkich musi być zdefiniowany, czasami będzie to 0, ale może to być NULL itd.
+
+# row = np.array([0, 2, 2, 0, 1, 2])
+# col = np.array([0, 0, 1, 2, 2, 2])
+# data = np.array([1, 2, 3, 4, 5, 6])
+# sc.csc_matrix((data, (row, col)), shape=(3, 3)).todense() ??????????????
+#
+
+# 6
+# rcv1 = fetch_rcv1()
+# X = rcv1["data"]
+# Y = rcv1.target[:87]
+# breakpoint()
