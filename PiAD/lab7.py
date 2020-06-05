764451302b659498d04613aab9da5eac77d1a672
lou000 <lewickimaciej1@gmail.com>
2020-06-05 21:00:13 +0200

    lab 7 prawie skonczone

--- PiAD/lab7.py
+++ PiAD/lab7.py
@@ -1,10 +1,14 @@
 from sklearn import datasets
+import scipy
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 from sklearn.metrics import jaccard_score
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from scipy.stats import mode
+import scipy.cluster.hierarchy as hr
 import numpy as np
 
 iris = datasets.load_iris()
@@ -63,40 +67,81 @@ print('\n', np.array(find_perm(3, Y, cluster_single.labels_)))
 
 print(jaccard_score(cluster_cmp.labels_, Y, average='micro'))
 
-pca = PCA(n_components=2)
-X_reduced = pca.fit_transform(X)
 
 
-def plot_PCA_2D(_pca, labels, name):
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_title(name, fontsize=20)
+def plot_PCA(data, labels, name):
+    _pca = PCA(n_components=2)
+    _X_reduced = _pca.fit_transform(data)
+    _fig = plt.figure(figsize=(16, 8))
+    _ax = _fig.add_subplot(1, 2, 1)
+    _ax.set_title(name, fontsize=20)
+    _ax2 = _fig.add_subplot(1, 2, 2, projection='3d')
 
     targets = [0, 1, 2]
     colors = ['r', 'g', 'b']
     for target, color in zip(targets, colors):
         idx = labels == target
-        group = _pca[idx]
-        ax.scatter(group[:, 0], group[:, 1], c=color, s=50)
+        group = _X_reduced[idx]
+        grp2 = X[idx]
+        _ax.scatter(group[:, 0], group[:, 1], c=color, s=50)
+        _ax2.scatter3D(grp2[:, 0], grp2[:, 1], grp2[:, 2], c=color)
         hull = ConvexHull(group)
         for simplex in hull.simplices:
-            ax.plot(group[simplex, 0], group[simplex, 1], 'k-')
-    ax.grid()
-    plt.show()
+            _ax.plot(group[simplex, 0], group[simplex, 1], 'k-')
+    _ax.grid()
+    # plt.show()
+
 
 
 complete_labels = np.array(find_perm(3, Y, cluster_cmp.labels_))
-plot_PCA_2D(X_reduced, Y, 'Oryginał')
-plot_PCA_2D(X_reduced, complete_labels, 'Complete')
+plot_PCA(X, Y, 'Oryginał')
+plot_PCA(X, complete_labels, 'Complete')
 
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(1, 1, 1)
+fig = plt.figure(figsize=(16, 8))
+ax = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2, projection='3d')
 ax.set_title("Różnice", fontsize=20)
 diff = Y == complete_labels
+
+pca = PCA(n_components=2)
+X_reduced = pca.fit_transform(X)
 for i, point in enumerate(X_reduced):
     if not diff[i]:
         ax.plot(point[0], point[1], 'ro')
+        ax2.scatter3D(X[i, 0], X[i, 1], X[i, 2], c="red")
     else:
         ax.plot(point[0], point[1], 'go')
+        ax2.scatter3D(X[i, 0], X[i, 1], X[i, 2], c="green")
 ax.grid()
-plt.show()
+# plt.show()
+# plt.show()
+
+Z = hr.linkage(X, method='complete')
+hr.dendrogram(Z)
+# plt.show()
+
+# 3
+from PIL import Image
+# Open the image form working directory
+image = Image.open('cat.jpg')
+im_data = np.asarray(image)
+size = image.size
+print(size)
+im_data = np.reshape(im_data, (size[0]*size[1], 3))
+image.show()
+
+# c_sizes = [2, 3, 5, 10, 30, 100]
+c_sizes = [10, 5, 3, 2]
+for s in c_sizes:
+    cluster_KMeans = KMeans(n_clusters=s, init='random').fit(im_data)
+    centers = cluster_KMeans.cluster_centers_
+    labels = cluster_KMeans.labels_
+    data2 = im_data.copy()
+    for i in range(s-1):
+        for j in range(size[0]*size[1]):
+            if i == labels[j]:
+                data2[j] = centers[i]
+    data2 = np.reshape(data2, (size[1], size[0], 3))
+    image2 = Image.fromarray(data2)
+    image2.show()
+    # cluster_AC = AgglomerativeClustering(n_clusters=s, linkage='single').fit(im_data)
