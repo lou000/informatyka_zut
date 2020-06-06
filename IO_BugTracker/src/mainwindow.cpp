44918a82222b11983d1f734c3a888ecd94ce9184
lou000 <44552514+lou000@users.noreply.github.com>
2020-06-06 02:25:34 +0200

    Initial setup.
    Created mainwindow and imported window and style dependancies.

---
+++ IO_BugTracker/src/mainwindow.cpp
@@ -0,0 +1,15 @@
+﻿#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowIcon(QIcon());
+    this->setWindowTitle("BugTracker   -   Project IO   -   by Lewicki Maciej and Jakub Kościołowski");
+    this->setFixedSize(QSize(1800, 1000));
+    this->setContentsMargins(10, 10, 10, 10);
+}
+
+MainWindow::~MainWindow()
+{
+}
+
