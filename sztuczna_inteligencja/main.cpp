65bef6ea7373f989640c039050c544e21e57716d
lou000 <lou000@users.noreply.github.com>
2021-03-27 00:31:24 +0100

    Update main.cpp

--- sztuczna_inteligencja/main.cpp
+++ sztuczna_inteligencja/main.cpp
@@ -13,26 +13,26 @@ int main()
 
 
 
-//    for(int i=0; i<1; i++)
-//    {
-//        std::wcout<<"\n\n"<<"BOARD 9x9:\n";
-//        Sudoku sud = Sudoku(3, puzzle9x9[i]);
-//        std::wcout<<"String: "<<puzzle9x9[i]<<"\n";
-//        std::wcout<<"Hash  : "<<sud.hash_code()<<"\n\n";
-//        std::wcout<<sud.to_string();
+    for(int i=0; i<5; i++)
+    {
+        std::wcout<<"\n\n"<<"BOARD 9x9:\n";
+        Sudoku sud = Sudoku(3, puzzle9x9[i]);
+        std::wcout<<"String: "<<puzzle9x9[i]<<"\n";
+        std::wcout<<"Hash  : "<<sud.hash_code()<<"\n\n";
+        std::wcout<<sud.to_string();
 
-//        std::wcout<<"\n\n"<<"SOLUTION:\n";
+        std::wcout<<"\n\n"<<"SOLUTION:\n";
 
 
-//        auto searcher = informative_searcher(sud, &Sudoku::compare, 10);
-//        for(size_t i = 0; i<searcher.get_number_of_solutions(); i++)
-//        {
-//            const Sudoku* solution = dynamic_cast<const Sudoku*>(searcher.get_solution(0));
-//            std::wcout<<solution->to_string();
-//        }
-//        std::wcout<<searcher.get_stats();
-//        std::wcout<<"\n\n"<<"==============================================================================================================\n";
-//    }
+        auto searcher = informative_searcher(sud, &Sudoku::compare, 10);
+        for(size_t i = 0; i<searcher.get_number_of_solutions(); i++)
+        {
+            const Sudoku* solution = dynamic_cast<const Sudoku*>(searcher.get_solution(0));
+            std::wcout<<solution->to_string();
+        }
+        std::wcout<<searcher.get_stats();
+        std::wcout<<"\n\n"<<"==============================================================================================================\n";
+    }
 
     for(int i=0; i<2; i++)
     {
