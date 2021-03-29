fa622469d22f3e0e03d32d98afa79b366fc07826
lou000 <lou000@users.noreply.github.com>
2021-03-29 20:34:01 +0200

    asd

--- sztuczna_inteligencja/SlidePuzzle/slidepuzzle.cpp
+++ sztuczna_inteligencja/SlidePuzzle/slidepuzzle.cpp
@@ -3,7 +3,9 @@
 SlidePuzzle::SlidePuzzle(uint16 n, SlidePuzzle::Heuristic heuristic)
     :n(n), size(n*n), heuristic(heuristic)
 {
-
+    ASSERT(n!=0);
+    createGrid();
+    shuffleGrid();
 }
 
 SlidePuzzle::SlidePuzzle(uint16 n, uint16 *grid, SlidePuzzle::Heuristic heuristic)
@@ -12,12 +14,46 @@ SlidePuzzle::SlidePuzzle(uint16 n, uint16 *grid, SlidePuzzle::Heuristic heuristi
 
 }
 
-bool SlidePuzzle::swapCells(uint16 indx1, uint16 indx2)
+std::vector<uint16> SlidePuzzle::getMovableCells(uint16 emptyIndx)
 {
-    auto& cell1 = grid[indx1];
-    auto& cell2 = grid[indx2];
+    ASSERT(n!=0);
+    std::vector<uint16> vec;
+    int col = emptyIndx % n;
+    int row = emptyIndx / n;
+    if(col!=0)
+        vec.push_back(emptyIndex - 1);
+    if(row!=0)
+        vec.push_back(emptyIndex - n);
+    if(col!=n-1)
+        vec.push_back(emptyIndex + 1);
+    if(row!=n-1)
+        vec.push_back(emptyIndex + n);
+    ASSERT(vec.size()>=2);
 
+    return vec;
+}
 
+void SlidePuzzle::swapWithEmpty(uint16 indx)
+{
+    auto& cell = grid[indx];
+    auto& empty = grid[emptyIndex];
+    ASSERT(empty == 0, "Empty index is not empty!\empty: grid[%d] = %d", emptyIndex, empty);
+#ifdef PUZZLE_DEBUG
+    int emptyRow = emptyIndex / n;
+    int cellRow  = indx / n;
+    int emptyCol = emptyIndex % n;
+    int cellCol  = indx % n;
+
+    ASSERT(emptyCol == cellCol || emptyRow == cellRow, "Cells must be on the same rows or columns!\n"
+                                     "empty: %d (%d,%d),  cell: %d = (%d,%d)",
+                                      emptyIndex, emptyCol, emptyRow, indx, cellCol, cellRow);
+    if(emptyCol == cellCol)
+        ASSERT(abs(emptyIndex-indx) == n);
+    else if(emptyRow == cellRow)
+        ASSERT(abs(emptyIndex-indx) == 1);
+#endif
+    std::swap(cell, empty);
+    emptyIndex = indx;
 }
 
 void SlidePuzzle::createGrid()
@@ -25,11 +61,16 @@ void SlidePuzzle::createGrid()
     for(int i=1; i<size-1; i++)
         grid[i] = i;
     grid[size-1] = 0;
+    emptyIndex = size-1;
 }
 
 void SlidePuzzle::shuffleGrid()
 {
-
+    for(int i=0; i<NUMBER_OF_SHUFFLES; i++)
+    {
+        auto cells = getMovableCells(emptyIndex);
+        int
+    }
 }
 
 bool SlidePuzzle::compare(const graph_state &a, const graph_state &b)
