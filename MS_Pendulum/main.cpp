cbc1101ec586a344d780992499c965b62ac14ba6
lou000 <44552514+lou000@users.noreply.github.com>
2021-04-18 13:21:50 +0200

    better

--- MS_Pendulum/main.cpp
+++ MS_Pendulum/main.cpp
@@ -21,13 +21,13 @@ class Pendulum : public olc::PixelGameEngine
 {
     double theta = PI/2;
     double vel = 0;
-    double len = 2;
-    double mass = 50;
-    double drag = 10;
-    double g = 9.81;
+    double len = 3;
+    double mass = 10;
+    double drag = 1;
+    double g = 10;
     double t = 0;
     float particleSpawnTime = 0;
-    int scale = 1;
+    int scale = 150;
     float centerX = 0;
     std::vector<Particle> particles;
 
@@ -40,7 +40,7 @@ public:
     bool OnUserCreate() override
     {
         SetPixelMode(olc::Pixel::ALPHA);
-        scale = GetDrawTargetWidth()/4;
+//        scale = GetDrawTargetWidth()/4;
         centerX = GetDrawTargetWidth()/2;
 
         return true;
@@ -49,44 +49,55 @@ public:
     bool OnUserUpdate(float dt) override
     {
         handleInput();
-        simulateStep(dt);
+        auto pos = simulateStep(0.0001, dt);
+        spawnParticles(pos, dt);
         Clear(olc::VERY_DARK_GREY);
         updateAndDrawParticles(dt);
-        drawPendulum();
+        drawPendulum(pos);
         return true;
     }
 
 private:
-    void simulateStep(float dt)
+    olc::vf2d simulateStep(double simStep, float dt)
     {
         //Runge-Kutta 4th order
-        double k11 = dt*vel;
-        double k12 = -dt*(drag/mass)*vel - dt*(g/len)*sin(theta);
+        int steps = dt/simStep;
+        double left = dt - steps*simStep;
 
-        double k21 = dt*(vel + k11/2);
-        double k22 = -dt*(drag/mass)*(vel + k12/2) - dt*(g/len)*sin(theta + k12/2);
+        for(int i=0; i<=steps; i++)
+        {
+            if(i==steps)
+                simStep = left;
 
-        double k31 = dt*(vel + k21/2);
-        double k32 = -dt*(drag/mass)*(vel + k22/2) -dt*(g/len)*sin(theta + k22/2);
+            double k11 = simStep*vel;
+            double k12 = -simStep*(drag/mass)*vel - simStep*(g/len)*sin(theta);
 
-        double k41 = dt*(vel + k31);
-        double k42 =  -dt*(drag/mass)*(vel + k32) - dt*(g/len)*sin(theta + k32);
+            double k21 = simStep*(vel + k11/2);
+            double k22 = -simStep*(drag/mass)*(vel + k12/2) - simStep*(g/len)*sin(theta + k12/2);
 
-        theta = theta + (k11 + 2*k21 + 2*k31 + k41)/6;
-        vel = vel + (k12 + 2*k22 + 2*k32 + k42)/6;
+            double k31 = simStep*(vel + k21/2);
+            double k32 = -simStep*(drag/mass)*(vel + k22/2) -simStep*(g/len)*sin(theta + k22/2);
+
+            double k41 = simStep*(vel + k31);
+            double k42 =  -simStep*(drag/mass)*(vel + k32) - simStep*(g/len)*sin(theta + k32);
+
+            theta = theta + (k11 + 2*k21 + 2*k31 + k41)/6;
+            vel = vel + (k12 + 2*k22 + 2*k32 + k42)/6;
+        }
+
         t+=dt;
 
-        double x = (double)GetDrawTargetWidth()/2 + sin(theta) * len * scale;
-        double y = cos(theta) * len * scale;
-        spawnParticles(x, y);
+
+        olc::vf2d pos;
+        pos.x = (double)GetDrawTargetWidth()/2 + sin(theta) * len * scale;
+        pos.y = cos(theta) * len * scale;
+        return pos;
     }
-    void drawPendulum()
+    void drawPendulum(olc::vf2d pos)
     {
         auto midX = GetDrawTargetWidth()/2;
-        double x = (double)GetDrawTargetWidth()/2 + sin(theta) * len * scale;
-        double y = cos(theta) * len * scale;
-        DrawLine(midX, 0, (int32_t)x, (int32_t)y);
-        FillCircle((int32_t)x, (int32_t)y, scale/4, olc::Pixel(214, 173, 92));
+        DrawLine(midX, 0, (int32_t)pos.x, (int32_t)pos.y);
+        FillCircle((int32_t)pos.x, (int32_t)pos.y, scale/4, olc::Pixel(214, 173, 92));
     }
     void handleInput()
     {
@@ -97,17 +108,16 @@ private:
             vel = 0;
         }
     }
-    void spawnParticles(int x, int y)
+    void spawnParticles(olc::vf2d pos, float dt)
     {
-        if(t - particleSpawnTime < 0.05) return;
-        int randVel = (int)abs(vel*500000)+1;
+        int randVel = (int)abs(vel*1500*dt)+1;
         for(int i=0; i<(rand() % randVel)-1; i++)
         {
             Particle p;
-            p.pos.x = x + rand() % scale/3 - scale/6;
-            p.pos.y = y + rand() % scale/3 - scale/6;
+            p.pos.x = pos.x + rand() % scale/3 - scale/6;
+            p.pos.y = pos.y + rand() % scale/3 - scale/6;
             p.size = rand() % (scale/40 + 1) * abs(vel);
-            p.vel = olc::vf2d(centerX - x, 0 - y).perp().norm()*10 * vel * len * ((double)rand()/RAND_MAX*2.0 + 0.5);
+            p.vel = olc::vf2d(centerX - pos.x, 0 - pos.y).perp().norm()*10 * vel * len * ((double)rand()/RAND_MAX*2.0 + 0.5);
             p.startS = p.size;
             p.lifeTime = (double)rand()/RAND_MAX*100.0 + 1.0;
             p.startLT = p.lifeTime;
@@ -152,7 +162,7 @@ int main()
 {
     ShowWindow(GetConsoleWindow(), SW_HIDE);
     Pendulum demo;
-    if (demo.Construct(400, 300, 1, 1, false, true))
+    if (demo.Construct(1000, 600, 1, 1, false, false))
         demo.Start();
     return 0;
 }
