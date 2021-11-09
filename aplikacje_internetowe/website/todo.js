f44e915252c1b1600629a98f7ca1fb42fe8a91e5
lou000 <44552514+lou000@users.noreply.github.com>
2021-11-09 20:37:35 +0100

    cleanup and comments

--- aplikacje_internetowe/website/todo.js
+++ aplikacje_internetowe/website/todo.js
@@ -1,3 +1,16 @@
+var mainTable = document.getElementById("mainTable")
+
+var codeInput = document.getElementById("inputCode");
+var taskInput = document.getElementById("inputTask");
+var dateInput = document.getElementById("inputDate");
+
+var codeSearch = document.getElementById("codeSearch")
+var taskSearch = document.getElementById("taskSearch")
+var dateSearch = document.getElementById("dateSearch")
+
+var alertBox  = document.getElementById("alert")
+var alertText = document.getElementById("alertText")
+
 class Task {
     constructor(code, task, date) 
     {
@@ -12,10 +25,20 @@ Date.prototype.isValid = function ()
     return this.getTime() === this.getTime();
 };
 
-var mainTable = document.getElementById("mainTable")
-var alertBox = document.getElementById("alert")
-var alertText = document.getElementById("alertText")
+// Fire filterElements when user types
+codeSearch.addEventListener('input', function (evt) {
+    filterElements();
+});
 
+taskSearch.addEventListener('input', function (evt) {
+    filterElements();
+});
+
+dateSearch.addEventListener('input', function (evt) {
+    filterElements();
+});
+
+// Hide alert when animation finishes
 alertBox.addEventListener("animationend", (ev) => {
     if (ev.type === "animationend" && ev.animationName === "fadeOut") 
         alertBox.style.display = "none";
@@ -24,13 +47,14 @@ alertBox.addEventListener("animationend", (ev) => {
 function showAlert(text)
 {
     alertText.innerHTML = text;
-    alertBox.style.display = "block";
-    alertBox.classList.toggle('active');
+    alertBox.style.display = "block"; // show element
+    alertBox.classList.toggle('active'); // start animation
 }
 
 function alertClose()
 {
-    alertBox.classList.toggle('active');
+    alertBox.classList.toggle('active'); // start animation
+    // at the end of animation eventListener fires and hides the alert
 }
 
 function validateInput(code, task, date)
@@ -66,110 +90,6 @@ function validateInput(code, task, date)
     if(!valid)
         showAlert(errorText)
     return valid;
-}
-
-function restoreRows()
-{
-    // Go through all the tasks in storage and create rows
-    var tasks = JSON.parse(localStorage.getItem("tasks"));
-    for(let i = 0; i<tasks.length; i++)
-        mainTable.appendChild(createRow(tasks[i].code, tasks[i].task, tasks[i].date));
-}
-
-
-var codeSearch = document.getElementById("codeSearch")
-var taskSearch = document.getElementById("taskSearch")
-var dateSearch = document.getElementById("dateSearch")
-
-codeSearch.addEventListener('input', function (evt) {
-    filterElements();
-});
-taskSearch.addEventListener('input', function (evt) {
-    filterElements();
-});
-dateSearch.addEventListener('input', function (evt) {
-    filterElements();
-});
-
-function filterElements()
-{
-    var b = document.getElementById("clearSearchBtn")
-    if(window.getComputedStyle(b).display === "none")
-    {
-        filterElementsMobile();
-        return;
-    }
-
-    var code = codeSearch.value;
-    var task = taskSearch.value;
-    var date = dateSearch.value;
-
-    for (let i = 0; i<mainTable.children.length; i++)
-        mainTable.children[i].style.display = "";
-
-    // Hide elements that match code
-    if(code !== "")
-        for (let i = 0; i<mainTable.children.length; i++) 
-        {
-            if(!mainTable.children[i].children[0].textContent.toUpperCase().includes(code.toUpperCase()))
-                mainTable.children[i].style.display = "none";
-        }
-
-    // Hide elements that match task
-    if(task !== "")
-        for (let i = 0; i<mainTable.children.length; i++) 
-        {
-            if(!mainTable.children[i].children[1].textContent.toUpperCase().includes(task.toUpperCase()))
-                mainTable.children[i].style.display = "none";
-        }
-
-    // Hide elements that match date
-    date = new Date(date)
-    if(date.isValid())
-    {
-        date = date.toLocaleDateString()
-        for (let i = 0; i<mainTable.children.length; i++) 
-        {
-            var rowDate = new Date(mainTable.children[i].children[2].id).toLocaleDateString();
-            if(rowDate !== date)
-                mainTable.children[i].style.display = "none";
-        }
-    }
-}
-
-function filterElementsMobile()
-{
-    var search = taskSearch.value;
-
-    for (let i = 0; i<mainTable.children.length; i++)
-        mainTable.children[i].style.display = "";
-    
-    if(search !== "")
-    {
-        for (let i = 0; i<mainTable.children.length; i++) 
-        {
-            var matches = false;
-            if(mainTable.children[i].children[0].textContent.toUpperCase().includes(search.toUpperCase()))
-                matches = true;
-            if(mainTable.children[i].children[1].textContent.toUpperCase().includes(search.toUpperCase()))
-                matches = true;
-            if(mainTable.children[i].children[2].textContent.toUpperCase().includes(search.toUpperCase()))
-                matches = true;
-            if(!matches)
-                mainTable.children[i].style.display = "none";
-        }
-    }
-}
-
-function clearSearch()
-{
-    // Clear search input and restore all rows
-    codeSearch.value = ""
-    taskSearch.value = ""
-    dateSearch.value = ""
-
-    for (let i = 0; i<mainTable.children.length; i++)
-        mainTable.children[i].style.display = "";
 }
 
 function createRow(code, task, date)
@@ -219,11 +139,98 @@ function createRow(code, task, date)
     return row
 }
 
+function filterElements()
+{
+    // if the clearSearchBtn is not displayed then
+    // we have only the taskSearch input, run filterElementsMobile
+    var b = document.getElementById("clearSearchBtn")
+    if(window.getComputedStyle(b).display === "none")
+    {
+        filterElementsMobile();
+        return;
+    }
+
+    var code = codeSearch.value;
+    var task = taskSearch.value;
+    var date = dateSearch.value;
+
+    // restore the rows
+    for (let i = 0; i<mainTable.children.length; i++)
+        mainTable.children[i].style.display = "";
+
+    // Hide elements that match code
+    if(code !== "")
+        for (let i = 0; i<mainTable.children.length; i++) 
+        {
+            if(!mainTable.children[i].children[0].textContent.toUpperCase().includes(code.toUpperCase()))
+                mainTable.children[i].style.display = "none";
+        }
+
+    // Hide elements that match task
+    if(task !== "")
+        for (let i = 0; i<mainTable.children.length; i++) 
+        {
+            if(!mainTable.children[i].children[1].textContent.toUpperCase().includes(task.toUpperCase()))
+                mainTable.children[i].style.display = "none";
+        }
+
+    // Hide elements that match date
+    date = new Date(date)
+    if(date.isValid())
+    {
+        date = date.toLocaleDateString()
+        for (let i = 0; i<mainTable.children.length; i++) 
+        {
+            var rowDate = new Date(mainTable.children[i].children[2].id).toLocaleDateString();
+            if(rowDate !== date)
+                mainTable.children[i].style.display = "none";
+        }
+    }
+}
+
+// For mobile displays we have only single search input
+// we search one phrase in all the rows
+function filterElementsMobile()
+{
+    var search = taskSearch.value;
+
+    // restore the rows
+    for (let i = 0; i<mainTable.children.length; i++)
+        mainTable.children[i].style.display = "";
+    
+    // hide the rows not matching our search
+    if(search !== "")
+    {
+        for (let i = 0; i<mainTable.children.length; i++) 
+        {
+            var matches = false;
+            if(mainTable.children[i].children[0].textContent.toUpperCase().includes(search.toUpperCase()))
+                matches = true;
+            if(mainTable.children[i].children[1].textContent.toUpperCase().includes(search.toUpperCase()))
+                matches = true;
+            if(mainTable.children[i].children[2].textContent.toUpperCase().includes(search.toUpperCase()))
+                matches = true;
+            if(!matches)
+                mainTable.children[i].style.display = "none";
+        }
+    }
+}
+
+function clearSearch()
+{
+    // Clear search input and restore all rows
+    codeSearch.value = ""
+    taskSearch.value = ""
+    dateSearch.value = ""
+
+    for (let i = 0; i<mainTable.children.length; i++)
+        mainTable.children[i].style.display = "";
+}
+
 function updateLocalStorage()
 {
     // We do this everytime we make any change, this is incredibly inefficiant
     // but no user will ever create enough tasks for it to matter
-    const mainTable = document.getElementById("mainTable")
 
     var tasks = []
     for (let i = 0; i<mainTable.children.length; i++) 
@@ -237,6 +244,14 @@ function updateLocalStorage()
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+function restoreRows()
+{
+    // Go through all the tasks in storage and create rows
+    var tasks = JSON.parse(localStorage.getItem("tasks"));
+    for(let i = 0; i<tasks.length; i++)
+        mainTable.appendChild(createRow(tasks[i].code, tasks[i].task, tasks[i].date));
+}
+
 function removeElement() 
 {
     var div = this.parentElement.parentElement;
@@ -246,20 +261,22 @@ function removeElement()
 
 function addElement()
 {
-    var mainTable = document.getElementById("mainTable")
-    var code = document.getElementById("inputCode").value;
-    var task = document.getElementById("inputTask").value;
-    var date = document.getElementById("inputDate").value;
+    var code = codeInput.value;
+    var task = taskInput.value;
+    var date = dateInput.value;
     if(!validateInput(code, task, date))
         return;
-    if(!editedRow)
-        mainTable.appendChild(createRow(code, task, date));
-    else
+
+    // If the user is editing the row we replace it, otherwise add new row
+    if(editedRow)
     {
         editedRow.parentNode.replaceChild(createRow(code, task, date), editedRow);
         clearInput();
     }
-    updateLocalStorage();
+    else
+        mainTable.appendChild(createRow(code, task, date));
+    
+        updateLocalStorage();
 }
 
 function editElement()
@@ -268,8 +285,8 @@ function editElement()
     editedRow.style.backgroundColor = "#A4E5EB";
     btn = document.getElementById("addButton");
     btn.textContent = "Apply"
-    document.getElementById("inputCode").value = editedRow.children[0].textContent;
-    document.getElementById("inputTask").value = editedRow.children[1].textContent;
+    codeInput.value = editedRow.children[0].textContent;
+    taskInput.value = editedRow.children[1].textContent;
     date = new Date(editedRow.children[2].id);
     
     // This below is the date conversion that is required by the <input type="date">
@@ -279,7 +296,7 @@ function editElement()
     var day = ('0' + date.getDate()).slice(-2) 
     var godHelpMe = date.getFullYear()+"-"+month+"-"+day;
 
-    document.getElementById("inputDate").value = godHelpMe;
+    dateInput.value = godHelpMe;
 }
 
 function stopEditing()
@@ -295,9 +312,9 @@ function stopEditing()
 
 function clearInput()
 {
-    document.getElementById("inputCode").value = ""
-    document.getElementById("inputTask").value = ""
-    document.getElementById("inputDate").value = ""
+    codeInput.value = ""
+    taskInput.value = ""
+    dateInput.value = ""
     stopEditing();
 }
 
